8cd716a38673102155d16d89efec2cc7502a55b0
wintercounter <wintercounter@gmail.com>
2017-09-14 01:04:32 +0200

    Make it happen

--- tvjs.js
+++ tvjs.js
@@ -1,8 +1,20 @@
-function gs(c) {
-    const c = function(){}
-    return {
-        
-    };
+import hyper, {wire} from './node_modules/hyperhtml/min.js';
+
+function gs() {
+    const o =  {
+        onclick(f = function () {console.log('onclick')}) {
+            const {name:n} = f;
+            const na = 'handleClick';
+            o[n ? n : na] = f;
+            console.info('onclick', o);
+            return o
+        },
+        get render() {
+            return hyper(document.body)
+        }
+    }
+
+    return o
 }
 
 let c = Object.defineProperty({}, 'g', {
@@ -10,16 +22,31 @@ let c = Object.defineProperty({}, 'g', {
     set: gs
 });
 
-object.define pr
-
 const v = c.g;
 
-const cmp = v;
+const t = v;
 
-cmp.
+const jajj = 'h';
 
+t
+    .onclick(function handleClick2() {console.log(2)})
+    .render`<a href="#" onclick=${t.handleClick2}>${jajj}bgfh</a>`
+
+
+//hyper(document.body)`<a href="#" onclick="${e => console.log(e)}">${jajj}bgfh</a>`
+
+console.log(t, t.handleClick);
+
+console.log('fut');
+
+export default 1;
 
 /*
 (`
     <button onclick=${this.handleClick}>
 `);*/
+
+// TODO
+/**
+ * ES6 Dynamic imports for handlers on call.
+ */
