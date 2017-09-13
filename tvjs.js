c7da9d21ecb72ba37168273330c6a7ad430e8837
wintercounter <wintercounter@gmail.com>
2017-09-13 22:14:52 +0200

    Initial commit

---
+++ tvjs.js
@@ -0,0 +1,25 @@
+function gs(c) {
+    const c = function(){}
+    return {
+        
+    };
+}
+
+let c = Object.defineProperty({}, 'g', {
+    get: gs,
+    set: gs
+});
+
+object.define pr
+
+const v = c.g;
+
+const cmp = v;
+
+cmp.
+
+
+/*
+(`
+    <button onclick=${this.handleClick}>
+`);*/
