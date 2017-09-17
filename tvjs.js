c9d4ead30710f019b61ca42cf47b5294b2cba092
wintercounter <wintercounter@gmail.com>
2017-09-17 23:57:48 +0200

    Make it happen

--- tvjs.js
+++ tvjs.js
@@ -1,19 +1,57 @@
-import hyper, {wire} from './node_modules/hyperhtml/min.js';
+import hyper, {wire, Component} from './node_modules/hyperhtml/min.js';
 
 function gs() {
+    const _c = Symbol()
+    const _s = Symbol();
+    const _d = Symbol();
+    const _tpl = Symbol();
+    const _ts = Object.prototype.toString
     const o =  {
+        [_s] : {},
+        state(s = {}) {
+            Object.assign(o[_s], s);
+            return o
+        },
         onclick(f = function () {console.log('onclick')}) {
-            const {name:n} = f;
-            const na = 'handleClick';
-            o[n ? n : na] = f;
-            console.info('onclick', o);
+            const {name:n} = f
+            const na = 'handleClick'
+            o[n ? n : na] = f
+            console.info('onclick', o)
             return o
         },
         get render() {
-            return hyper(document.body)
+            return function(d = document.body) {
+                return function(...a) {
+                    o[_c] = class extends Component {
+                        get defaultState() {return o[_s]}
+                        static get _tpl() { return a }
+                        render() {
+                            o[_s] = this.state
+                            return this.html(...a)
+                        }
+                    }
+
+                    o[_d] = hyper(
+                        _ts.call(d) === "[object String]"
+                            ? document.querySelector(d)
+                            : d
+                    )`${new o[_c]}`
+                    return o
+                }
+            }
+        },
+        blackhole(fn) {
+            _ts.call(fn) === '[object Function]' && fn()
+            return o
+        },
+        testAfter(){
+            console.log('after')
+            return o
+        },
+        get wire() {
+            return wire;
         }
     }
-
     return o
 }
 
@@ -22,24 +60,10 @@ let c = Object.defineProperty({}, 'g', {
     set: gs
 });
 
-const v = c.g;
+export default c.g;
 
-const t = v;
+/////////////////////////////////////////////////////
 
-const jajj = 'h';
-
-t
-    .onclick(function handleClick2() {console.log(2)})
-    .render`<a href="#" onclick=${t.handleClick2}>${jajj}bgfh</a>`
-
-
-//hyper(document.body)`<a href="#" onclick="${e => console.log(e)}">${jajj}bgfh</a>`
-
-console.log(t, t.handleClick);
-
-console.log('fut');
-
-export default 1;
 
 /*
 (`
